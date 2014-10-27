92b201345f730110445d83f4fefe8ae88bc4872b
tsilva08 <thiagomagelat@yahoo.com.br>
2014-10-27 17:32:44 -0200

    Move methods class Tiger to Feline.

--- src/org/felines/Feline.java
+++ src/org/felines/Feline.java
@@ -2,6 +2,23 @@ package org.felines;
 
 public class Feline {
 
+	
+	private int age;
+	private int name;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+	
 	public void meow() {
 		System.out.println("meow");
 	}
