bbd8dc082406a950adf73b7211c887bfab6480f1
tsilva08 <thiagomagelat@yahoo.com.br>
2014-10-27 18:56:10 -0200

    Create AnimalMarilho. Extract SuperClass.

---
+++ src/org/reptile/AnimalMarilho.java
@@ -0,0 +1,9 @@
+package org.reptile;
+
+public class AnimalMarilho {
+
+	public AnimalMarilho() {
+		super();
+	}
+
+}
--- src/org/reptile/Reptile.java
+++ src/org/reptile/Reptile.java
@@ -1,6 +1,6 @@
 package org.reptile;
 
-public class Reptile {
+public class Reptile extends AnimalMarilho {
 
 	protected int age;
 	protected int name;
