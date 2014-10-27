0e193b7d02902c6f2abf7c88eebe937d1ac5fc51
tsilva08 <thiagomagelat@yahoo.com.br>
2014-10-27 19:06:38 -0200

    Extract Method AnimalMarilho to Reptile

--- src/org/reptile/Reptile.java
+++ src/org/reptile/Reptile.java
@@ -25,6 +25,17 @@ public class Reptile extends AnimalMarilho {
 		return true;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		result = prime * result + age;
+		result = prime * result + name;
+		result = prime * result + spead;
+		return result;
+	}
+
 	
 
 }
